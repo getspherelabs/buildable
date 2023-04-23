400e865f9933bc5d3ffef5fb16c74cc16f1b6a97
behzoddev <behzoddev@gmail.com>
2023-04-24 06:29:19 +0900

    Add BuildableFactoryProcessorProvider

---
+++ buildable-processors/src/main/java/io/spherelabs/buildableprocessors/BuildableFactoryProcessorProvider.kt
@@ -0,0 +1,15 @@
+package io.spherelabs.buildableprocessors
+
+import com.google.devtools.ksp.processing.SymbolProcessor
+import com.google.devtools.ksp.processing.SymbolProcessorEnvironment
+import com.google.devtools.ksp.processing.SymbolProcessorProvider
+
+class BuildableFactoryProcessorProvider : SymbolProcessorProvider {
+
+    override fun create(environment: SymbolProcessorEnvironment): SymbolProcessor {
+        return BuildableFactoryProcessor(
+            logger = environment.logger,
+            codeGenerator = environment.codeGenerator
+        )
+    }
+}
