341c285269d2e1c87f80d15354d7af3dff6e2338
Behzod Halil <behzoddev@gmail.com>
2023-04-25 16:14:29 +0900

    Improve BuildableFactoryProcessor

--- buildable-processors/src/main/java/io/spherelabs/buildableprocessors/BuildableFactoryProcessor.kt
+++ buildable-processors/src/main/java/io/spherelabs/buildableprocessors/BuildableFactoryProcessor.kt
@@ -7,66 +7,27 @@ import com.google.devtools.ksp.symbol.KSClassDeclaration
 import com.google.devtools.ksp.symbol.KSNode
 import com.google.devtools.ksp.validate
 import com.squareup.kotlinpoet.ClassName
-import com.squareup.kotlinpoet.FileSpec
-import com.squareup.kotlinpoet.FunSpec
-import com.squareup.kotlinpoet.TypeSpec
 import com.squareup.kotlinpoet.ksp.toClassName
 import com.squareup.kotlinpoet.ksp.writeTo
 import io.spherelabs.buildableannotations.BuildableComponent
-import io.spherelabs.buildableannotations.BuildableFactory
 
 class BuildableFactoryProcessor(
     private val logger: KSPLogger,
-    private val codeGenerator: CodeGenerator
+    private val codeGenerator: CodeGenerator,
+    private val factoryGenerator: Generator,
+    private val factoryFinder: Finder
 ) : SymbolProcessor {
 
     override fun process(resolver: Resolver): List<KSAnnotated> {
         logger.info("BuildableFactoryProcessor was invoked.")
-        val factories = factories(resolver)
+        val factories = factoryFinder(resolver)
         val components = components(resolver, factories)
 
         components.forEach {
-            fileGeneration(it.key, it.value).writeTo(codeGenerator, Dependencies(true))
+            factoryGenerator(it.key, it.value).writeTo(codeGenerator, Dependencies(true))
         }
 
         return emptyList()
-    }
-
-    private fun factories(resolver: Resolver): Set<ClassName> {
-        return resolver.getSymbolsWithAnnotation(BuildableFactory::class.qualifiedName.orEmpty())
-            .filterIsInstance<KSClassDeclaration>()
-            .filter(KSNode::validate)
-            .map { it.toClassName() }
-            .toSet()
-    }
-
-    private fun fileGeneration(key: ClassName, list: List<ClassName>): FileSpec {
-        val packageName = key.packageName
-        val funcName = key.simpleName + "Factory"
-        val enumName = key.simpleName + "Type"
-
-        return FileSpec.builder(packageName, funcName)
-            .addType(
-                TypeSpec.enumBuilder(enumName)
-                    .apply {
-                        list.forEach {
-                            addEnumConstant(it.simpleName.uppercase())
-                        }
-                    }
-                    .build())
-            .addFunction(
-                FunSpec.builder(funcName)
-                    .addParameter("key", ClassName(packageName, enumName))
-                    .returns(key)
-                    .beginControlFlow("return when (key)")
-                    .apply {
-                        list.forEach {
-                            addStatement("${enumName}.${it.simpleName.uppercase()} -> %T()", it)
-                        }
-                    }
-                    .endControlFlow()
-                    .build())
-            .build()
     }
 
     private fun components(
