e35fa0af547a260b4cf7c8155d97fbbe0276f92b
Behzod Halil <behzoddev@gmail.com>
2023-04-25 16:16:47 +0900

    Add BuildableMapperProcessorProvider

---
+++ buildable-processors/mapper/src/main/java/io/behzod/mapper/BuildableMapperProcessorProvider.kt
@@ -0,0 +1,15 @@
+package io.behzod.mapper
+
+import com.google.devtools.ksp.processing.SymbolProcessor
+import com.google.devtools.ksp.processing.SymbolProcessorEnvironment
+import com.google.devtools.ksp.processing.SymbolProcessorProvider
+
+class BuildableMapperProcessorProvider : SymbolProcessorProvider {
+
+    override fun create(environment: SymbolProcessorEnvironment): SymbolProcessor {
+        return BuildableMapperProcessor(
+            codeGenerator = environment.codeGenerator,
+            logger = environment.logger
+        )
+    }
+}
