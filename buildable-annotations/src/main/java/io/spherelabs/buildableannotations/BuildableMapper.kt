191937fed03310e4a958196b433b8aa0ebb79c00
Behzod Halil <behzoddev@gmail.com>
2023-04-25 16:14:01 +0900

    Update settings & gradle

--- buildable-annotations/src/main/java/io/spherelabs/buildableannotations/BuildableMapper.kt
+++
@@ -1,5 +0,0 @@
-package io.spherelabs.buildableannotations
-
-@Target(AnnotationTarget.CLASS)
-@Retention(AnnotationRetention.SOURCE)
-annotation class BuildableMapper
---
+++ buildable-processors/factory/src/test/java/io/behzod/factory/ExampleUnitTest.kt
@@ -0,0 +1,3 @@
+package io.behzod.factory
+
+
